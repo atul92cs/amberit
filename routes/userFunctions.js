9b519b8f7c4edc8c108a08b9b3ab7b349fdcaaf3
Atul <atul.11192@gmail.com>
2019-03-29 03:43:41 +0530

    adding login function to the backend

--- routes/userFunctions.js
+++ routes/userFunctions.js
@@ -16,7 +16,7 @@ router.post('/register',(req,res)=>{
        Status
      }).then(()=>{
         res.status(201).json({
-          message:'User reegistered'
+          message:'User registered'
         });
      }).catch(err=>{
        res.status(501).json({
@@ -25,5 +25,38 @@ router.post('/register',(req,res)=>{
        });
      });
    });
+});
+router.post('/login',(req,res)=>{
+  const Email=req.body.email;
+  let fetchedUser;
+  model.User.findOne({where:{Email}}).then(user=>{
+    if(!user)
+    {
+      res.status(402).json({
+        message:"User not found"
+      });
+    }
+    fetchedUser=user;
+    return bcrypt.compare(req.body.password,user.Password);
+  }).then(result=>{
+    if(!result)
+    {
+      res.status(403).json({
+        message:"Error occured"
+      });
+    }
+    const token=jwt.sign({email:fetchedUser.Email},"Jackward",{expiresIn:"72h"});
+    res.status(201).json({
+      token:token,
+      expiresIn:72000,
+      email:fetchedUser.Email,
+      name:fetchedUser.Name,
+      status:fetchedUser.Status
+    });
+  }).catch(err=>{
+       res.status(406).json({
+         message:"Server error"
+       });
+  });
 });
 module.exports=router;
